feb155a7cc5d97f2d5a4325ffa0e2a96ef9b32b0
gui816 <g.guerreiro844@gmail.com>
2026-06-14 18:23:30 +0100

    PWA: manifest, PNG icons 192/512, SW cache, meta tags

--- static/sw.js
+++ static/sw.js
@@ -1,2 +1,31 @@
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", (e) => e.waitUntil(clients.claim()));
+const CACHE = "nutrilens-v1";
+const ASSETS = [
+  "/",
+  "/static/index.html",
+  "/static/logo-square.svg",
+  "/static/icon-192x192.png",
+  "/static/icon-512x512.png",
+  "/static/manifest.json"
+];
+
+self.addEventListener("install", (e) => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(ASSETS)).catch(() => {})
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
+});
